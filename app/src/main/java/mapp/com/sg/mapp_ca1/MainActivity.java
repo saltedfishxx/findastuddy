bb067ea26800ece8639f53ca4d15ff6a92baab37
Brian Teo <briandesaw@gmail.com>
2018-07-22 19:54:19 +0800

    Added in fragments for each icons pressed for bottom nav bar

--- app/src/main/java/mapp/com/sg/mapp_ca1/MainActivity.java
+++ app/src/main/java/mapp/com/sg/mapp_ca1/MainActivity.java
@@ -1,14 +1,23 @@
 package mapp.com.sg.mapp_ca1;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.bignerdranch.expandablerecyclerview.Model.ParentObject;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,24 +27,63 @@ import mapp.com.sg.mapp_ca1.Adapter.MainAdapter;
 
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
+    //Create Fragments
+    private HomeFragment homeFragment;
+    private BrowseFragment browseFragment;
+    private ProfileFragment profileFragment;
 
-
+    private BottomNavigationViewEx btmNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = (RecyclerView)findViewById(R.id.mRecyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        MainAdapter adapter = new MainAdapter(this);
 
 
-        recyclerView.setAdapter(adapter);
+
+        //Initialize Fragments
+        homeFragment = new HomeFragment();
+        browseFragment = new BrowseFragment();
+        profileFragment = new ProfileFragment();
+
+        btmNav = (BottomNavigationViewEx) findViewById(R.id.btmNav);
+        btmNav.setTextSize(15);
+        btmNav.setIconSize(30,30);
+        btmNav.enableAnimation(true);
+        btmNav.enableShiftingMode(true);
+        btmNav.enableItemShiftingMode(true);
+
+
+
+        btmNav.setOnNavigationItemSelectedListener(new BottomNavigationViewEx.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.action_browse:
+                        setFragment(browseFragment);
+                        break;
+                    case R.id.action_home:
+                        setFragment(homeFragment);
+                        break;
+                    case R.id.action_profile:
+                        setFragment(profileFragment);
+                        break;
+                }
+                return true;
+            }
+        });
+
 
 
     }
+
+    //Change screen based on fragment parsed in
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_screen, fragment);
+        fragmentTransaction.commit();
+    }
+
 
 }
