e3d35e719bd9a523b8748918eb1dad0ee9e25166
Brian Teo <briandesaw@gmail.com>
2018-08-04 16:52:26 +0800

    Material design works but not done

--- app/src/main/java/mapp/com/sg/mapp_ca1/CreateMeetup.java
+++ app/src/main/java/mapp/com/sg/mapp_ca1/CreateMeetup.java
@@ -9,24 +9,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+
+import java.util.Date;
+
 import mapp.com.sg.mapp_ca1.Firestore.MeetupFirestoreHelper;
 import mapp.com.sg.mapp_ca1.Models.Meetup;
 
 public class CreateMeetup extends AppCompatActivity{
-    private EditText meetupName;
+    private EditText name;
     private Button meetupDateAndTime, meetupLocation, createMeetup;
+
+    private Date dateTime;
+    private String groupChatID;
+    private GeoPoint location;
+    private String meetupName;
+    private int noPpl;
+
+    MeetupFirestoreHelper meetupFirestoreHelper;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meetup);
 
-        meetupName = (EditText) findViewById(R.id.editMeetup);
+        name = (EditText) findViewById(R.id.editMeetup);
         meetupDateAndTime = (Button) findViewById(R.id.btnMeetupDateTime);
         meetupLocation = (Button) findViewById(R.id.btnMeetupLocation);
         createMeetup = (Button) findViewById(R.id.btnCreateMeetup);
 
-        MeetupFirestoreHelper meetupFirestoreHelper;
+
 
     }
 
@@ -36,12 +49,13 @@ public class CreateMeetup extends AppCompatActivity{
             switch(v.getId()){
                 //TODO: code methods for each buttons' onclick
                 case(R.id.btnMeetupDateTime):
-
+                    Intent i = new Intent(getApplicationContext(),PickDateTime.class);
+                    startActivity(i);
                     break;
 
                 case(R.id.btnMeetupLocation):
-                    Intent i = new Intent(getApplicationContext(),PickLocation.class);
-                    startActivity(i);
+                    Intent i2 = new Intent(getApplicationContext(),PickLocation.class);
+                    startActivity(i2);
                     break;
 
                 case(R.id.btnCreateMeetup):
@@ -53,6 +67,10 @@ public class CreateMeetup extends AppCompatActivity{
     };
 
     private void AddMeetUp() {
+        final String TAG = "MeetupFirestoreHelper";
+        meetupFirestoreHelper = new MeetupFirestoreHelper();
+        meetupName = name.getText().toString();
+
 
 
     }
