3e6bbd9b8d6d0827723ec39143537373fba2e5de
Brian Teo <briandesaw@gmail.com>
2018-08-04 11:24:32 +0800

    Added MeetupFirestoreHelper and Meetup Model

--- app/src/main/java/mapp/com/sg/mapp_ca1/CreateMeetup.java
+++ app/src/main/java/mapp/com/sg/mapp_ca1/CreateMeetup.java
@@ -1,12 +1,16 @@
 
 package mapp.com.sg.mapp_ca1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import mapp.com.sg.mapp_ca1.Firestore.MeetupFirestoreHelper;
+import mapp.com.sg.mapp_ca1.Models.Meetup;
 
 public class CreateMeetup extends AppCompatActivity{
     private EditText meetupName;
@@ -21,30 +25,35 @@ public class CreateMeetup extends AppCompatActivity{
         meetupDateAndTime = (Button) findViewById(R.id.btnMeetupDateTime);
         meetupLocation = (Button) findViewById(R.id.btnMeetupLocation);
         createMeetup = (Button) findViewById(R.id.btnCreateMeetup);
+
+        MeetupFirestoreHelper meetupFirestoreHelper;
+
     }
 
-//    View.OnClickListener clickListener = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            switch(v.getId()){
-//                //TODO: code methods for each buttons' onclick
-//                case(R.id.btnMeetupDateTime):
-//
-//                    break;
-//
-//                case(R.id.btnMeetupLocation):
-//
-//                    break;
-//
-//                case(R.id.btnCreateMeetup):
-//                    AddMeetUp();
-//
-//
-//            }
-//        }
-//    };
-//
-//    private Object AddMeetUp() {
-//
-//    }
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                //TODO: code methods for each buttons' onclick
+                case(R.id.btnMeetupDateTime):
+
+                    break;
+
+                case(R.id.btnMeetupLocation):
+                    Intent i = new Intent(getApplicationContext(),PickLocation.class);
+                    startActivity(i);
+                    break;
+
+                case(R.id.btnCreateMeetup):
+                    AddMeetUp();
+
+
+            }
+        }
+    };
+
+    private void AddMeetUp() {
+
+
+    }
 }
---
+++ app/src/main/java/mapp/com/sg/mapp_ca1/Firestore/MeetupFirestoreHelper.java
@@ -0,0 +1,96 @@
+package mapp.com.sg.mapp_ca1.Firestore;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import mapp.com.sg.mapp_ca1.CreateMeetup;
+import mapp.com.sg.mapp_ca1.Models.Meetup;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class MeetupFirestoreHelper {
+    static CollectionReference meetupCollection = FirebaseFirestore.getInstance().collection("Meetup");
+    List<Meetup> listMeetup;
+    Meetup meetup;
+
+    public MeetupFirestoreHelper(CreateMeetup cm){
+        final CreateMeetup createMeetup = cm;
+
+        meetupCollection
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        listMeetup = new ArrayList<>();
+                        if(task.isSuccessful()){
+                            for(DocumentSnapshot document : task.getResult()){
+                                String meetId = document.getId();
+                                Date date = document.getDate("date");
+                                String groupChatId = document.getString("groupChatID");
+                                GeoPoint location = document.getGeoPoint("location");
+                                String meetupName = document.getString("meetupName");
+                                int noPpl = (int) document.get("noPpl");
+
+                                Meetup meetup = new Meetup(meetId,date,groupChatId,location,meetupName,noPpl);
+                            }
+                        }else{
+                            Log.d(TAG,"Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public MeetupFirestoreHelper(){
+
+    }
+
+    public void saveData(Meetup m){
+        Map<String,Object> data = new HashMap<>();
+        data.put("date", m.getDateTime());
+        data.put("groupChatId",m.getGroupChatID());
+        data.put("location",m.getLocation());
+        data.put("meetupName",m.getMeetupName());
+        data.put("noPpl",m.getNoPpl());
+        meetupCollection.document(m.getMeetId()).set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("FirestoreHelper","Document has been saved");
+            }
+        });
+    }
+
+    public void deleteMeetup(Meetup m){
+        meetupCollection.document(m.getMeetId()).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "Document has been deleted");
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error while deleting document");
+                    }
+                });
+
+
+    }
+
+
+}
