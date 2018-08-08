f8c2c68d36239b3d1fe18242e98ec9f848912e19
Brian Teo <briandesaw@gmail.com>
2018-08-09 02:10:43 +0800

    Meetup Logic is done

--- app/src/main/java/mapp/com/sg/mapp_ca1/CreateMeetup.java
+++ app/src/main/java/mapp/com/sg/mapp_ca1/CreateMeetup.java
@@ -1,37 +1,63 @@
 
 package mapp.com.sg.mapp_ca1;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
-import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
+import java.security.acl.Group;
 import java.sql.Time;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+
 import mapp.com.sg.mapp_ca1.Firestore.MeetupFirestoreHelper;
+import mapp.com.sg.mapp_ca1.Models.GroupChats;
 import mapp.com.sg.mapp_ca1.Models.Meetup;
+
 
 public class CreateMeetup extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
     private EditText name;
-    private Button meetupDateAndTime, meetupLocation, createMeetup;
+    private static Button meetupDateAndTime, meetupLocation, createMeetup;
 
 
-    private Date dateTime;
-    private String groupChatID;
+    private Date meetupDateTime;
+    private String groupChatID, dateDisplay, timeDisplay;
     private GeoPoint location;
     private String meetupName;
     private int noPpl;
+    private GroupChats selectedChatId;
+    private DatePickerDialog datePickerDialog;
+    private TimePickerDialog timePickerDialog;
+    private Calendar mDate, mTime;
+    private int PLACE_PICKER_REQUEST = 1;
+
 
     MeetupFirestoreHelper meetupFirestoreHelper;
 
@@ -39,6 +65,10 @@ public class CreateMeetup extends AppCompatActivity implements DatePickerDialog.
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meetup);
+
+        Bundle bundle = getIntent().getExtras();
+        selectedChatId = (GroupChats) bundle.getSerializable("selectedChats");
+
 
         name = (EditText) findViewById(R.id.editMeetup);
         meetupDateAndTime = (Button) findViewById(R.id.btnMeetupDateTime);
@@ -73,45 +103,111 @@ public class CreateMeetup extends AppCompatActivity implements DatePickerDialog.
 //        }
 //    };
 
+    //Adding meetup to FireStore
     private void AddMeetUp() {
         final String TAG = "MeetupFirestoreHelper";
         meetupFirestoreHelper = new MeetupFirestoreHelper();
         meetupName = name.getText().toString();
+        //TODO: get groupChatId of group that created the meetup
+        //Converting date and time into a timestamp
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
+        String date = dateFormat.format(mDate.getTime());
+        final SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm");
+        String time = timeFormat.format(mTime.getTime());
+        String dateTime = (date + time);
+        try{
+            final SimpleDateFormat finalFormat = new SimpleDateFormat("dd/MM/yy hh:mm");
+            Date parsedDate = finalFormat.parse(dateTime);
+            Timestamp timestamp = new java.sql.Timestamp(parsedDate.getTime());
+            meetupDateTime = timestamp;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        noPpl = 1;
+        Meetup meetup = new Meetup(null,meetupDateTime,selectedChatId.toString(),location,meetupName,noPpl);
+        meetupFirestoreHelper.saveData(meetup);
+
+
+
+
+
 
 
 
     }
 
+
+
     public void onClickDate(View view) {
-        Intent i = new Intent(getApplicationContext(),PickDateTime.class);
-        startActivity(i);
+        Calendar mCurrentDate = Calendar.getInstance();
+        int mYear = mCurrentDate.get(Calendar.YEAR);
+        int mMonth = mCurrentDate.get(Calendar.MONTH);
+        int mDay = mCurrentDate.get(Calendar.DAY_OF_MONTH);
+
+        Calendar mCurrentTime = Calendar.getInstance();
+        int hour = mCurrentTime.get(Calendar.HOUR);
+        int minute = mCurrentTime.get(Calendar.MINUTE);
+
+
+
+        datePickerDialog = new DatePickerDialog(CreateMeetup.this, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT, CreateMeetup.this, mYear,mMonth,mDay);
+        datePickerDialog.show();
+        timePickerDialog = new TimePickerDialog(CreateMeetup.this, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT,CreateMeetup.this,hour,minute,false);
+        timePickerDialog.show();
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        meetupDateAndTime.setText(dayOfMonth + "/" + (month + 1) + "/" + year + "   " + meetupDateAndTime.getText());
+        mDate = Calendar.getInstance();
+        mDate.set(year,month,dayOfMonth);
     }
 
     public void onClickLocation(View view) {
-        Intent i2 = new Intent(getApplicationContext(),PickLocation.class);
-        startActivity(i2);
+//        Intent i = new Intent(getApplicationContext(),PickLocation.class);
+//        startActivity(i);
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        Intent intent;
+        try {
+            intent = builder.build(CreateMeetup.this);
+            startActivityForResult(intent,PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == PLACE_PICKER_REQUEST)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                Place place = PlacePicker.getPlace(data,this);
+                String address = String.format("%s", place.getAddress());
+                location = new GeoPoint(place.getLatLng().latitude, place.getLatLng().longitude);
+                meetupLocation.setText(address);
+            }
+        }
     }
 
 
-    @Override
-    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
-        final Calendar c = Calendar.getInstance();
-        year = c.get(Calendar.YEAR);
-        monthOfYear = c.get(Calendar.MONTH);
-        dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
-
-        Date date = new Date(year,monthOfYear,dayOfMonth);
-
-    }
 
     public void onClickCreateMeet(View view) {
         AddMeetUp();
     }
 
+
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        Date time = Calendar.getInstance().getTime();
+        mTime = Calendar.getInstance();
+        mTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        mTime.set(Calendar.MINUTE, minute);
 
+        meetupDateAndTime.setText(String.format("%02d:%02d",hourOfDay ,minute ));
 
     }
 }
--- app/src/main/java/mapp/com/sg/mapp_ca1/DatePickerFragment.java
+++ app/src/main/java/mapp/com/sg/mapp_ca1/DatePickerFragment.java
@@ -1,30 +1,26 @@
 package mapp.com.sg.mapp_ca1;
 
-import android.app.Fragment;
-import android.graphics.Color;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
-import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
-
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DatePickerFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
-    private TextView dateTextView;
-    private DatePickerDialog dpd;
 
     public DatePickerFragment() {
         // Required empty public constructor
@@ -32,60 +28,25 @@ public class DatePickerFragment extends Fragment implements DatePickerDialog.OnD
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_pick_date, container, false);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
 
-        // Find our View instances
-        dateTextView = view.findViewById(R.id.date_textview);
-        Button dateButton = view.findViewById(R.id.date_button);
-
-
-
-        // Show a datepicker when the dateButton is clicked
-        dateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar now = Calendar.getInstance();
-                /*
-                It is recommended to always create a new instance whenever you need to show a Dialog.
-                The sample app is reusing them because it is useful when looking for regressions
-                during testing
-                 */
-                if (dpd == null) {
-                    dpd = DatePickerDialog.newInstance(
-                            DatePickerFragment.this,
-                            now.get(Calendar.YEAR),
-                            now.get(Calendar.MONTH),
-                            now.get(Calendar.DAY_OF_MONTH)
-                    );
-                } else {
-                    dpd.initialize(
-                            DatePickerFragment.this,
-                            now.get(Calendar.YEAR),
-                            now.get(Calendar.MONTH),
-                            now.get(Calendar.DAY_OF_MONTH)
-                    );
-                }
-
-                dpd.show(getFragmentManager(), "Datepickerdialog");
-            }
-        });
-
-        return view;
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        DatePickerDialog dpd = (DatePickerDialog) getFragmentManager().findFragmentByTag("Datepickerdialog");
-        if(dpd != null) dpd.setOnDateSetListener(this);
-    }
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        String date = day + "/" + month + "/" + year;
+        Intent i = new Intent(getActivity().getBaseContext(),CreateMeetup.class);
+        i.putExtra("dateMeetup", date);
+        getActivity().startActivity(i);
 
-    @Override
-    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
-        String date = "You picked the following date: "+dayOfMonth+"/"+(++monthOfYear)+"/"+year;
-        dateTextView.setText(date);
+
+
 
     }
 }
